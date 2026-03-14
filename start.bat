e397319da1ae6e1b0f5443a04d008c7583346fdf
Aleksey Schumann <aleksey.schumann@gmail.com>
2026-03-14 16:40:02 +0300

    add optuna optimizer & mlflow results

--- start.bat
+++ start.bat
@@ -1,7 +1,16 @@
 @echo off
 
 cd /d "%~dp0"
+
+echo Starting MLflow UI
+start "MLflow Server" cmd /c "call venv\Scripts\activate && mlflow ui"
+
+echo Waiting for MLflow to start
+timeout /t 5 /nobreak > nul
+start http://127.0.0.1:5000
+
+echo Starting Jupyter Notebook
 call venv\Scripts\activate
-jupyter notebook
+jupyter notebook agent.ipynb
 
 @REM pause
